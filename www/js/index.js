8ffd3cd1843d74cdaa2b3be3172b80595a517a40
MosaicStrategiesGroup <mosaic@Mosaics-Mac-mini-Joe.local>
2016-03-25 10:49:45 -0400

    Pushbots updates

--- www/js/index.js
+++ www/js/index.js
@@ -17,27 +17,34 @@
  * under the License.
  */
 var app = {
+	
     // Application Constructor
     initialize: function() {
-        this.bindEvents();
+        app.bindEvents();
     },
+	
     // Bind Event Listeners
     //
     // Bind any events that are required on startup. Common events are:
     // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
+        document.addEventListener('deviceready', app.onDeviceReady, false);
     },
+	
     // deviceready Event Handler
     //
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-		var Pushbots = PushbotsPlugin.initialize("56e085f717795979748b4567", {"android":{"sender_id":"745856988600"}});
-        app.receivedEvent('deviceready');
+		document.addEventListener('deviceready', function () {
+			var Pushbots = PushbotsPlugin.initialize("56e085f717795979748b4567", {"android":{"sender_id":"745856988600"}});
+		}, false);
+		app.receivedEvent('deviceready');
     },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
+	
+	// Update DOM on a Received Event
+	receivedEvent: function(id) {
+		
         var parentElement = document.getElementById(id);
         var listeningElement = parentElement.querySelector('.listening');
         var receivedElement = parentElement.querySelector('.received');
@@ -46,5 +53,26 @@ var app = {
         receivedElement.setAttribute('style', 'display:block;');
 
         console.log('Received Event: ' + id);
-    }
+    
+		// Should be called once app receive the notification
+		Pushbots.on("notification:received", function(data){
+			console.log("received:" + JSON.stringify(data));
+		});
+		
+		// Should be called once the notification is clicked
+		// **important** Doesn't work with iOS while app is closed
+		Pushbots.on("notification:clicked", function(data){
+			console.log("clicked:" + JSON.stringify(data));
+		});
+		
+		// Should be called once the device is registered successfully with Apple or Google servers
+		Pushbots.on("registered", function(token){
+			console.log(token);
+		});
+		
+		Pushbots.getRegistrationId(function(token){
+			console.log("Registration Id:" + token);
+		});
+		
+	}
 };
