5838a628917e9000c13d16686542b814a17f3a34
JoeMosaic <joe@mosaicstg.com>
2021-07-06 11:23:08 -0400

    Remove and re-add pushbots js

--- www/js/index.js
+++ www/js/index.js
@@ -41,11 +41,38 @@ var app = {
 
       window.plugins.PushbotsPlugin.initialize( "56e085f717795979748b4567", { "android": { "sender_id": "720859137196" } } );
 
+      // Only with First time registration
+      window.plugins.PushbotsPlugin.on("registered", function(token){
+        console.log("Registration Id:" + token);
+      });
+
+      //Get user registrationId/token and userId on PushBots, with evey launch of the app even launching with notification
+      window.plugins.PushbotsPlugin.on("user:ids", function(data){
+        console.log("user:ids" + JSON.stringify(data));
+      });
+
       //iOS only
       //Reset Badge
       window.plugins.PushbotsPlugin.resetBadge();
       //Set badge
-      window.plugins.PushbotsPlugin.setBadge( 0 );
+      window.plugins.PushbotsPlugin.setBadge(0);
+
+      // Should be called once app receive the notification [foreground/background]
+      window.plugins.PushbotsPlugin.on("notification:received", function(data){
+        console.log("received:" + JSON.stringify(data));
+
+        //iOS: [foreground/background]
+        console.log("notification received from:" + data.cordova_source);
+        //Silent notifications Only [iOS only]
+        //Send CompletionHandler signal with PushBots notification Id
+        window.plugins.PushbotsPlugin.done(data.pb_n_id);
+      });
+
+      window.plugins.PushbotsPlugin.on("notification:clicked", function(data){
+        // var userToken = data.token;
+        // var userId = data.userId;
+        console.log("clicked:" + JSON.stringify(data));
+      });
 
       app.receivedEvent( 'deviceready' );
 
@@ -59,17 +86,7 @@ var app = {
         console.log( "clicked:" + JSON.stringify( data ) );
       } );
 
-      // Should be called once the device is registered successfully with Apple or Google servers
-      window.plugins.PushbotsPlugin.on( "registered", function ( token ) {
-        console.log( token );
-      } );
-
-      window.plugins.PushbotsPlugin.getRegistrationId( function ( token ) {
-        console.log( "Registration Id:" + token );
-      } );
-
     }, false );
-
   },
 
   // Update DOM on a Received Event
