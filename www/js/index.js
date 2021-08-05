b693db3c33b1a4c2c29ebb8a7add800eadaa1ce5
JoeMosaic <joe@mosaicstg.com>
2021-08-05 17:52:46 -0400

    One Signal setup - iOS

--- www/js/index.js
+++ www/js/index.js
@@ -39,53 +39,6 @@ var app = {
 
     document.addEventListener( 'deviceready', function () {
 
-      window.plugins.PushbotsPlugin.initialize( "56e085f717795979748b4567", { "android": { "sender_id": "720859137196" } } );
-
-      // Only with First time registration
-      window.plugins.PushbotsPlugin.on("registered", function(token){
-        console.log("Registration Id:" + token);
-      });
-
-      //Get user registrationId/token and userId on PushBots, with evey launch of the app even launching with notification
-      window.plugins.PushbotsPlugin.on("user:ids", function(data){
-        console.log("user:ids" + JSON.stringify(data));
-      });
-
-      //iOS only
-      //Reset Badge
-      window.plugins.PushbotsPlugin.resetBadge();
-      //Set badge
-      window.plugins.PushbotsPlugin.setBadge(0);
-
-      // Should be called once app receive the notification [foreground/background]
-      window.plugins.PushbotsPlugin.on("notification:received", function(data){
-        console.log("received:" + JSON.stringify(data));
-
-        //iOS: [foreground/background]
-        console.log("notification received from:" + data.cordova_source);
-        //Silent notifications Only [iOS only]
-        //Send CompletionHandler signal with PushBots notification Id
-        window.plugins.PushbotsPlugin.done(data.pb_n_id);
-      });
-
-      window.plugins.PushbotsPlugin.on("notification:clicked", function(data){
-        // var userToken = data.token;
-        // var userId = data.userId;
-        console.log("clicked:" + JSON.stringify(data));
-      });
-
-      app.receivedEvent( 'deviceready' );
-
-      // Should be called once app receive the notification
-      window.plugins.PushbotsPlugin.on( "notification:received", function ( data ) {
-        console.log( "received:" + JSON.stringify( data ) );
-      } );
-
-      // Should be called once the notification is clicked
-      window.plugins.PushbotsPlugin.on( "notification:clicked", function ( data ) {
-        console.log( "clicked:" + JSON.stringify( data ) );
-      } );
-
     }, false );
   },
 
@@ -99,3 +52,46 @@ var app = {
     receivedElement.setAttribute( 'style', 'display:block;' );
   }
 };
+
+var app = {
+  // Application Constructor
+  initialize: function() {
+    document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+  },
+
+  // deviceready Event Handler
+  //
+  // Bind any cordova events here. Common events are:
+  // 'pause', 'resume', etc.
+  onDeviceReady: function() {
+    this.receivedEvent('deviceready');
+  },
+
+  receivedEvent: function() {
+    //START ONESIGNAL CODE
+    //Remove this method to stop OneSignal Debugging
+    //window.plugins.OneSignal.setLogLevel({logLevel: 6, visualLevel: 0});
+
+    var notificationOpenedCallback = function(jsonData) {
+      console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
+    };
+    // Set your iOS Settings
+    var iosSettings = {};
+    iosSettings["kOSSettingsKeyAutoPrompt"] = false;
+    iosSettings["kOSSettingsKeyInAppLaunchURL"] = false;
+
+    window.plugins.OneSignal
+      .startInit("bb5931c0-b97c-405a-8fcd-728ab3538937")
+      .handleNotificationOpened(notificationOpenedCallback)
+      .iOSSettings(iosSettings)
+      .inFocusDisplaying(window.plugins.OneSignal.OSInFocusDisplayOption.Notification)
+      .endInit();
+
+    // The promptForPushNotificationsWithUserResponse function will show the iOS push notification prompt. We recommend removing the following code and instead using an In-App Message to prompt for notification permission (See step 6)
+    window.plugins.OneSignal.promptForPushNotificationsWithUserResponse(function(accepted) {
+      console.log("User accepted notifications: " + accepted);
+    });
+    //END ONESIGNAL CODE
+  }
+}
+app.initialize();
